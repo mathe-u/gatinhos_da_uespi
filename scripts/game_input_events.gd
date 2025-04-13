4608e4a54b8f84d14d61931990e6599bfd8be850
mathe-u <matheussilvades@gmail.com>
2025-04-12 23:04:18 -0300

    initiate game test scenes

---
+++ scripts/game_input_events.gd
@@ -0,0 +1,26 @@
+class_name GameInputEvents
+
+static var direction: Vector2
+
+static func movement_inut() -> Vector2:
+	if Input.is_action_pressed("mv_up"):
+		direction = Vector2.UP
+	elif Input.is_action_pressed("mv_down"):
+		direction = Vector2.DOWN
+	elif Input.is_action_pressed("mv_left"):
+		direction = Vector2.LEFT
+	elif Input.is_action_pressed("mv_right"):
+		direction = Vector2.RIGHT
+	else:
+		direction = Vector2.ZERO
+	
+	return direction
+	
+static func is_movement_input() -> bool:
+	if direction == Vector2.ZERO:
+		return false
+	return true
+
+static func use_tool() -> bool:
+	var use_tool_value = Input.is_action_just_pressed("hit")
+	return use_tool_value
