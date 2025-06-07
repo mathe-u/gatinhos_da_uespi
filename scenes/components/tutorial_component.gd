fdb6bdf37b485429d3cb8a9a7c28e99578352672
mathe-u <matheussilvades@gmail.com>
2025-06-07 11:56:18 -0300

    player light fix, tutorial text added

--- scenes/components/tutorial_component.gd
+++ scenes/components/tutorial_component.gd
@@ -1,14 +1,15 @@
 extends Node2D
 
-@export var text: String
+@export var text: String = ""
 
 @onready var interactable_component: InteractableComponent = $InteractableComponent
 @onready var box_label: PanelContainer = $BoxLabel
-@onready var interactable_label: TextureRect = $InteractableLabel
-@onready var label: Label = $BoxLabel/Label
+@onready var label: Label = $BoxLabel/MarginContainer/Label
+
 @onready var life_time: Timer = $LifeTime
 
-var in_range: bool = false
+var in_range: bool = true
+
 
 func _ready() -> void:
 	box_label.hide()
@@ -17,16 +18,14 @@ func _ready() -> void:
 	life_time.timeout.connect(_on_life_time_timeout)
 	
 	interactable_component.interactable_activated.connect(_on_interactable_activated)
-	interactable_component.interactable_deactivated.connect(_on_interactable_deactivated)
 
 
 func _on_interactable_activated() -> void:
 	box_label.show()
-	life_time.start()
-	in_range = true
-
-
-func _on_interactable_deactivated() -> void:
+	
+	if in_range:
+		life_time.start()
+	
 	in_range = false
 
 
