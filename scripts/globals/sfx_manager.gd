e3098e6887e9a71fd8b5c4b775dce3e76405efd3
mathe-u <matheussilvades@gmail.com>
2025-06-10 23:28:17 -0300

    sfx manager fix

--- scripts/globals/sfx_manager.gd
+++ scripts/globals/sfx_manager.gd
@@ -10,7 +10,7 @@ func play_click_sfx() -> void:
 	var click_sfx_scene: PackedScene = load("res://scenes/components/click_button_audio.tscn")
 	var click_sfx_instance: AudioStreamPlayer2D = click_sfx_scene.instantiate()
 	
-	click_sfx_instance.autoplay = true
+	click_sfx_instance.autoplay = false
 	click_sfx_instance.finished.connect(_on_audio_finished.bind(click_sfx_instance))
 	get_parent().add_child(click_sfx_instance)
 	
@@ -21,6 +21,5 @@ func play_main_menu_music() -> void:
 	pass
 
 func _on_audio_finished(audio_stream_player: AudioStreamPlayer2D) -> void:
-	
 	audio_stream_player.queue_free()
 	
